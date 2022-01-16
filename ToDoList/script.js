6588424b9b2305a6c1048f70c5fc6bf02a7bf6db
VS <90790481+Vshalee@users.noreply.github.com>
2022-01-16 07:38:02 -0500

    adds functions Clearlist and addListAfterEnter

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,14 +1,18 @@
 //global variables
 let ul = document.querySelector('ul');
-let input_txt = document.getElementById('user_input') //input from the input box
+let input_txt = document.getElementById('user_input') //input box
 let submit = document.querySelector('.submit')         // submit button
 let clear_list = document.querySelector('.clear_list')  // clear list button
 let btn = document.getElementsByClassName('button')
 
 function createListElement() {
-        
+    
+    if(!input_txt.value.trim()){   // if all white spaces are entered then trim() 
+        alert('Please enter a valid list item');
+        return;
+      }
         let list_node = document.createElement('li')
-        list_node.appendChild(document.createTextNode(input_txt.value));
+        list_node.appendChild(document.createTextNode(input_txt.value.trim()));
         user_input.value = ''
         let btn_x = document.createElement('button')
         btn_x.textContent = ' X '
@@ -31,25 +35,28 @@ function addListAfterClick() {
 
 }
 
-input_txt.addEventListener("keyup", function(event) {
-    if (event.key === 'Enter') {
-        event.preventDefault()
-        document.querySelector('.submit').click()
-    }
-}) 
+function addListAfterEnter(event) {
 
-clear_list.addEventListener('click', function () {
+    if (input_length() > 0 && event.key === 'Enter') {
+    
+        createListElement();
+}
+
+}
+
+function clearListAfterClick()
+{
     const menu = document.getElementById('menu')
     while (menu.firstChild) {
         
             menu.removeChild(menu.firstChild)
         }
     
-})
+}
 
 submit.addEventListener('click', addListAfterClick)
   
+input_txt.addEventListener('keypress', addListAfterEnter)
 
-
-    
+clear_list.addEventListener('click', clearListAfterClick)    
 
